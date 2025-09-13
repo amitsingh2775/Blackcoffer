ede208b444c359e213b4bc77bec10b1710dc7472
amit kumar <107244837+amitsingh2775@users.noreply.github.com>
2025-09-13 17:46:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   
-  origin: ['http://localhost:3000', 'http://localhost:5173'], 
+  origin: [ 'https://blackcoffer-kappa.vercel.app'], 
   credentials: true
 }));
 app.use(express.json());
